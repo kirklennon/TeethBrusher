4504fd40e2e195e21dab1edad18708bb4518c5bc
Kirk Lennon <kirklennon@me.com>
2022-12-03 08:31:15 -0800

    Initial Commit

---
+++ TeethBrusher/TeethBrusherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeethBrusherApp.swift
+//  TeethBrusher
+//
+//  Created by Kirk Lennon on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TeethBrusherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
